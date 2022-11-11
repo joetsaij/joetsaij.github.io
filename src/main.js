135e86bd601e048e7f7219ec0e07600cd6ec6bbf
OPENFIND\joe_tsai <joe_tsai@openfind.com.tw>
2022-11-11 12:36:07 +0800

    toast use default css

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 
 import VueToast from 'vue-toast-notification';
 // Import one of the available themes
-//import 'vue-toast-notification/dist/theme-default.css';
-import 'vue-toast-notification/dist/theme-sugar.css';
+import 'vue-toast-notification/dist/theme-default.css';
+// import 'vue-toast-notification/dist/theme-sugar.css';
+// import 'vue-toast-notification';
 Vue.use(VueToast, {
   // global options
 });
